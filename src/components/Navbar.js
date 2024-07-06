a0de1005c80117e5afb85307768562d978f730cf
jose Alfonzo <124316952+jrolando15@users.noreply.github.com>
2024-07-06 11:36:23 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,48 @@
 import React from 'react'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
-const NavbarContainer = styled.nav `
-  display: flex;
-  justify-content: center;
+const StyledNavbar = styled(Navbar)`
   background-color: #f8f8f8;
-  padding: 10px 0;
   border-bottom: 1px solid #ddd;
 `
 
-const NavLink = styled(Link)`
+const StyledNavLink = styled(Nav.Link)`
   margin: 0 15px;
   padding: 10px 20px;
   font-size: 1.2em;
-  color: #66bdea;
+  color: #66bdea !important;
   text-decoration: none;
   border-radius: 5px;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color:  #005f8d;
-    color : #fff;
+    background-color: #005f8d;
+    color: #fff !important;
   }
-
 
   &.active {
     background-color: #005f8d;
-    color: #fff;
+    color: #fff !important;
   }
 `
 
-const Navbar = () => (
-    <NavbarContainer>
-      <NavLink to="/" activeClassName="active">Home</NavLink>
-      <NavLink to="/programs" activeClassName="active">Programs</NavLink>
-      <NavLink to="/instructors" activeClassName="active">Instructors</NavLink>
-      <NavLink to="/schedule" activeClassName="active">Schedule</NavLink>
-      <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-    </NavbarContainer>
-  )
-  
-  export default Navbar
+const NavbarComponent = () => (
+  <StyledNavbar expand="lg">
+    <Container>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto">
+          <StyledNavLink as={Link} to="/" activeClassName="active">Home</StyledNavLink>
+          <StyledNavLink as={Link} to="/programs" activeClassName="active">Programs</StyledNavLink>
+          <StyledNavLink as={Link} to="/instructors" activeClassName="active">Instructors</StyledNavLink>
+          <StyledNavLink as={Link} to="/schedule" activeClassName="active">Schedule</StyledNavLink>
+          <StyledNavLink as={Link} to="/contact" activeClassName="active">Contact</StyledNavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </StyledNavbar>
+)
+
+export default NavbarComponent
