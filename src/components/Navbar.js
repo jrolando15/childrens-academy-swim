6bfd52c40d60a77e4b603e031c2688de830d2c7a
jose Alfonzo <124316952+jrolando15@users.noreply.github.com>
2024-07-09 10:08:39 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,26 @@ import styled from 'styled-components'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const StyledNavbar = styled(Navbar)`
-  background-color: #f8f8f8;
-  border-bottom: 1px solid #ddd;
+  background-color: #0d6efd;
+  border-bottom: 1px solid #0a58ca;
 `
 
 const StyledNavLink = styled(Nav.Link)`
   margin: 0 15px;
   padding: 10px 20px;
   font-size: 1.2em;
-  color: #66bdea !important;
+  color: #ffffff !important;
   text-decoration: none;
   border-radius: 5px;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #005f8d;
+    background-color: #0a58ca;
     color: #fff !important;
   }
 
   &.active {
-    background-color: #005f8d;
+    background-color: #0a58ca;
     color: #fff !important;
   }
 `
@@ -38,6 +38,7 @@ const NavbarComponent = () => (
           <StyledNavLink as={Link} to="/programs" activeClassName="active">Programs</StyledNavLink>
           <StyledNavLink as={Link} to="/instructors" activeClassName="active">Instructors</StyledNavLink>
           <StyledNavLink as={Link} to="/schedule" activeClassName="active">Schedule</StyledNavLink>
+          <StyledNavLink as={Link} to="/testimonials" activeClassName="active">Testimonials</StyledNavLink>
           <StyledNavLink as={Link} to="/contact" activeClassName="active">Contact</StyledNavLink>
         </Nav>
       </Navbar.Collapse>
@@ -45,4 +46,4 @@ const NavbarComponent = () => (
   </StyledNavbar>
 )
 
-export default NavbarComponent
+export default NavbarComponent;
